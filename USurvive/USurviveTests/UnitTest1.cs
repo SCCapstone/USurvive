4df1d3df7206458137b8a79b4729f64a5003ba2c
Joseph Still <thetechconspiracy@outlook.com>
2021-01-26 20:47:46 -0500

    Added first unit test

--- USurvive/USurviveTests/UnitTest1.cs
+++ USurvive/USurviveTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System;
+using System.Collections.Generic;
+using USurvive;
 
 namespace USurviveTests
 {
@@ -13,9 +15,20 @@ namespace USurviveTests
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                USurvive.Assignment 
+                string name = "Name";
+                string instructor = "Inst";
+                int creditHours = 5;
+                Uri instructorEmail = null;
+                Uri classWebsite = null;
+                USurvive.Syllabus syllabus = null;
+                int classType = 0;
+                string notes = "notes";
+                List<MeetingTime> meetingTimes = null;
 
+                //USurvive.Assignment assignment = new USurvive.Assignment(name, new USurvive.Class("Class name", null, 0, null, null, null, 0, "", null), new DateTime(), new DateTime(), 0, null, false, 0);
+                USurvive.Class UClass = new USurvive.Class(name, instructor, creditHours, instructorEmail,classWebsite, syllabus, classType, notes, meetingTimes);
 
+                Assert.IsTrue(UClass.Name.Equals(name));
             }
         }
     }
