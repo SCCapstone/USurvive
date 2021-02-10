24ac82785b4803442381a6ec3797eb8e82ca1b7d
Matthew McKee <mmmckee@email.sc.edu>
2021-02-09 20:04:46 -0500

    updated

--- USurvive/USurvive/MainWindow.xaml.cs
+++ USurvive/USurvive/MainWindow.xaml.cs
@@ -84,8 +84,8 @@ namespace USurvive
             SidebarFrame.Navigate(sidebar);
 
             //For now, open to the Classes view.  Eventually this will open to the Today view once that is implemented
-            ClassesView classesView = new ClassesView();
-            NavigationFrame.Navigate(classesView);
+            HomeView homeView = new HomeView();
+            NavigationFrame.Navigate(homeView);
         }
         private void DebugClick(Object sender, RoutedEventArgs e)
         {
