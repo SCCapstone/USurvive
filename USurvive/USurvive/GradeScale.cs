fa8daa0a6b509172331ac9617057c843efb1fa93
Joseph Still <thetechconspiracy@outlook.com>
2021-02-10 11:19:22 -0500

    Implemented a basic GradeScale class

--- USurvive/USurvive/GradeScale.cs
+++ USurvive/USurvive/GradeScale.cs
@@ -8,5 +8,21 @@ namespace USurvive
 {
     public class GradeScale
     {
+        int pointIncrement { get; set; }
+        int roundingType { get; set; }
+        char specialGrade { get; set; }//Used for unique grades, such as 'W' and 'I'
+
+        public GradeScale(int pointIncrement, int roundingType)
+        {
+            this.pointIncrement = pointIncrement;
+            //Eventually, add checks to ensure this is valid
+            this.roundingType = roundingType;
+        }
+
+        public char getLetterGrade(int gradeValue)
+        {
+            Console.WriteLine("Not implemented!");
+            return 'z';
+        }
     }
 }
